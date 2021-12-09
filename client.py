865f8a122b70f6b775e0e72fd25dbd126866d381
Kirill <202536@edu.fa.ru>
2021-12-09 22:53:53 +0300

    Задания для практикума

--- client.py
+++ client.py
@@ -1,14 +1,61 @@
 import socket
 import pickle
+import os
 
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 9090
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
-
 p, g, a = 7, 5, 3
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+K = 0
+B = 0
 
-sock.close()
+
+def encrypt(s, private, public):
+    return ''.join(map(chr, [(x+private+public) for x in map(ord, s)]))
+
+
+def has_keys_in_storage():
+    return os.path.isfile('client') and os.path.getsize('client') > 0
+
+
+def send_public_key():
+    sock.send(pickle.dumps((p, g, A)))
+
+
+def generate_public_key():
+    global K, B
+    ser = sock.recv(1024)
+    B = pickle.loads(ser)
+    print("Server public key received: ", B)
+    print("Generating client private key...")
+    K = B ** a % p
+    with open('client', 'wb') as f:
+        f.write(pickle.dumps((a, g, p, A, K)))
+
+
+def start_messaging():
+    sock.send(pickle.dumps(encrypt("server_check_keys", K, B)))
+
+    msg = sock.recv(1024)
+    port = encrypt(pickle.loads(msg), K, B)
+    sock.close()
+    print(f"Received messaging port: {port}. Going to this port")
+
+    s1 = socket.socket()
+    s1.connect((HOST, int(port)))
+    msg = ''
+    while msg != 'close':
+        msg = input('Your message: ')
+        s1.send(pickle.dumps(encrypt(msg, K, B)))
+    s1.close()
+
+
+if __name__ == '__main__':
+    send_public_key()
+    generate_public_key()
+    start_messaging()
+    sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,63 @@
 import socket
 import pickle
+import os
+import random
 
 HOST = '127.0.0.1'
-PORT = 8080
-
+PORT = 9090
+SERVER_MESSAGE_HELLO = 'server_check_keys'
 sock = socket.socket()
-sock.bind((HOST, PORT))
+sock.bind(('', PORT))
 sock.listen(1)
 conn, addr = sock.accept()
+b = 9
+A, B, K = 0, 0, 0
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
 
-conn.close()
+def decrypt(s, private, public):
+    return ''.join(map(chr, [(x-private-public) for x in map(ord, s)]))
+
+
+def has_keys_in_storage():
+    return os.path.isfile('client') and os.path.getsize('client') > 0
+
+
+def check_key():
+    global K, B, A
+    msg = conn.recv(1024)
+    p, g, A = pickle.loads(msg)
+    B = g ** b % p
+    K = A ** b % p
+    with open('server', 'wb') as f:
+        f.write(pickle.dumps((B, K)))
+    print(f"[server] Generated private and public keys")
+    conn.send(pickle.dumps(B))
+    msg = conn.recv(1024)
+    if decrypt(pickle.loads(msg), K, A) != SERVER_MESSAGE_HELLO:
+        print("[server] Wrong client key. Closing connection...")
+        conn.close()
+    else:
+        print("[server] Key validation success. Starting listening...")
+        start_serving()
+
+
+def start_serving():
+    new_port = random.randint(1000, 65536)
+    conn.send(pickle.dumps(decrypt(str(new_port), K, B)))
+    conn.close()
+
+    s1 = socket.socket()
+    s1.bind(('', new_port))
+    s1.listen(1)
+    conn1, addr1 = s1.accept()
+
+    while True:
+        data = conn1.recv(1024)
+        if not data:
+            break
+        print(f"[{addr1[0]}] {decrypt(pickle.loads(data), K, B)}")
+    conn1.close()
+
+
+if __name__ == '__main__':
+    check_key()
